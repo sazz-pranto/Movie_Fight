6fb3ba14ff361b7d8071ad38aafcbb7a73b71956
Sazzad Hossain <sazz.pranto96@gmail.com>
2021-03-09 23:37:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
+    return response.data.Search;
 }
 const inputElement = document.querySelector('input');
-const onInput = (event) => {
-    fetchData(event.target.value);
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value); //wait until fetchData can return the search results
+    for(let movie of movies) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `;
+    }
 }
 inputElement.addEventListener('input', debounce(onInput));
