c932a972af0ee3786db3fc603f6477aa501ec001
Sazzad Hossain <sazz.pranto96@gmail.com>
2021-03-11 22:52:47 +0600

    reuseable code for autocomplete functionality

---
+++ autocomplete.js
@@ -0,0 +1,58 @@
+const createAutoComplete = ({
+    rootElement, 
+    renderOption, 
+    onOptionSelect, 
+    inputValue,
+    fetchData
+}) => {
+    //HTML for autocomplete dropdown
+    rootElement.innerHTML = `
+        <lable><b>Search</b></lable>
+        <input class="input" />
+        <div class="dropdown">
+            <div class="dropdown-menu">
+                <div class="dropdown-content results"></div>
+            </div>
+        </div>
+    `;
+
+    const inputElement = rootElement.querySelector('.input');
+
+    const dropdown = rootElement.querySelector('.dropdown');
+    const resultsWrapper = rootElement.querySelector('.results');
+
+    //fetching search results and renderning the results into the dropdown
+    const onInput = async (event) => {
+        const items = await fetchData(event.target.value); //wait until fetchData can return the search results
+        if(!items.length) {
+            dropdown.classList.remove('is-active');
+            return;
+        }
+        resultsWrapper.innerHTML = ""; // clearing the previous results before searching for a new item
+        dropdown.classList.add('is-active'); //is-active class to activate the dropdown
+
+        //looping through the items
+        for(let item of items) {
+            // each item should be an anchor element 
+            const option = document.createElement('a');
+            option.classList.add('dropdown-item');
+            option.innerHTML = renderOption(item);
+            //appending the result as a child element of results
+            resultsWrapper.appendChild(option);
+            
+            //if there is a click on any result, remove the dropdown menu and update the input text with the corresponding item's title
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active');
+                inputElement.value = inputValue(item);
+                onOptionSelect(item);
+            });
+        }
+    }
+    inputElement.addEventListener('input', debounce(onInput)); //call debounce with a delay arg to set a delay, default 1000ms
+
+    // check if user clicks anywhere but the autocomplete widget to close the results bar 
+    document.addEventListener('click', event => {
+        if(!rootElement.contains(event.target)) dropdown.classList.remove('is-active');
+    });
+
+}
--- index.js
+++ index.js
@@ -1,22 +1,83 @@
-// fetch movie data from the API 
-const fetchData = async (searchTerm) => {
-    const response = await axios.get("http://www.omdbapi.com/", {
+//create autocomplete widgets with a configuration object passed as an argument
+createAutoComplete({
+    rootElement: document.querySelector('.autocomplete'),
+    renderOption(movie) {
+        const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster; //checking if any movie poster is available or not
+        return `
+            <img src="${imgSrc}" />
+            <span>${movie.Title}</span>
+            <span>&nbsp(${movie.Year})</span>
+        `;
+    },
+    onOptionSelect(movie) {
+        onMovieSelect(movie);
+    },
+    inputValue(movie) {
+        return movie.Title;
+    },
+
+    // fetch movie data from the OMDB API 
+    async fetchData(searchTerm) {
+        const response = await axios.get("http://www.omdbapi.com/", {
         params: {
             apikey: "529ca8bc",
             s: searchTerm
         }
-    });
-    return response.data.Search;
-}
-const inputElement = document.querySelector('input');
-const onInput = async (event) => {
-    const movies = await fetchData(event.target.value); //wait until fetchData can return the search results
-    for(let movie of movies) {
-        const div = document.createElement('div');
-        div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}</h1>
-        `;
+        });
+        if(response.data.Error) {
+            return [];
+        }
+        return response.data.Search;
     }
-}
-inputElement.addEventListener('input', debounce(onInput));
+});
+
+// send a followup request to the API and get details for the corresponding movie 
+const onMovieSelect = async movie => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "529ca8bc",
+            i: movie.imdbID
+        }
+    });
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+//HTML template for showing detailed info for any movie
+const movieTemplate = (movieDetails) => {
+    return `
+    <article class="media">
+        <figure class="media-left">
+            <p class="image">
+                <img src="${movieDetails.Poster}" />
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <h1>${movieDetails.Title}</h1>
+                <h4>${movieDetails.Genre}</h4>
+                <p>${movieDetails.Plot}</p>
+            </div>
+        </div>
+    </article>
+    <article class="notification is-primary">
+        <p class="title">${movieDetails.Awards}</p>
+        <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+        <p class="title">${movieDetails.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+        <p class="title">${movieDetails.Metascore}</p>
+        <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+        <p class="title">${movieDetails.imdbRating}</p>
+        <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+        <p class="title">${movieDetails.imdbVotes}</p>
+        <p class="subtitle">IMDB Votes</p>
+    </article>
+    `;
+};
