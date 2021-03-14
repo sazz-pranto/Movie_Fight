274c4c5d3d341d6fccc11bcacd5871be6edc7451
Sazzad Hossain <sazz.pranto96@gmail.com>
2021-03-14 18:35:32 +0600

    comparison

--- index.js
+++ index.js
@@ -72,17 +72,54 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 
 // method for showing comparison results
 const runComparison = () => {
-    console.log('Compared!');
+    console.log('in comparison')
+    //select all the elements containing the stats of the movie
+    //all elements with id of left/right-summary with corresponding element of notification class
+    const leftSideStat = document.querySelector('#left-summary').querySelectorAll('.notification');
+    const rightSideStat = document.querySelector('#right-summary').querySelectorAll('.notification');
+    leftSideStat.forEach((leftStat, index) => {  //index is being used to access the corresponsing stat at the right side
+        const rightStat = rightSideStat[index];
+
+        //dataset object is used to get a data attribute
+        const leftSideValue = leftStat.dataset.value;
+        const rightSideValue = rightStat.dataset.value;
+    });
 }
 
 //HTML template for showing detailed info for any movie
 const movieTemplate = (movieDetails) => {
-    //extracting numbers from different properties of the movie to compare easily
-    const dollars = parseInt(movieDetails.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+    //extracting numbers from different properties of the movie using RegEx to compare easily
+    const dollars = parseInt(movieDetails.BoxOffice.replace(/\$/g, '').replace(/,/g, '')); //first replaces the $ sign and then all the commas
     const metascore = parseInt(movieDetails.Metascore);
     const imdbRating = parseFloat(movieDetails.imdbRating);
-    const imdbVotes = parseInt(movieDetails.imdbVotes.replace(/,/g, ''));
-    console.log(dollars, metascore, imdbRating, imdbVotes);
+    const imdbVotes = parseInt(movieDetails.imdbVotes.replace(/,/g, '')); //replaces all the commas
+
+    /*calculating the total number of awards that a movie won
+    winner is the one who has a greater value for total awards
+    For Example, The Avengers => Nominated for 1 Oscar. Another 38 wins & 79 nominations. sum of awards=118 */
+    const awards = movieDetails.Awards.split(' ').reduce((previous, word) => {
+        const value = parseInt(word);
+        if(isNaN(value)) {
+            return previous;
+        } else {
+            return previous + value;
+        }
+    }, 0); //initial value is 0 as a second param in reduce()
+    //using forEach()
+    // let counter = 0;
+    // const awards = movieDetails.Awards.split(' ').forEach(word => {
+    //     const value = parseInt(word);
+    //     if(isNaN(value)) {
+    //         return;
+    //     } else {
+    //         counter = counter+ value;
+    //     }
+    // });
+
+
+    /*The data-* attribute is used to store custom data private to the page or application.
+    The data-* attribute gives us the ability to embed custom data attributes on all HTML elements.
+    Here is this HTML snippet, data-value attribute keeps a relevant data about its own article */
     return `
     <article class="media">
         <figure class="media-left">
@@ -98,23 +135,23 @@ const movieTemplate = (movieDetails) => {
             </div>
         </div>
     </article>
-    <article class="notification is-primary">
+    <article data-value="${awards}" class="notification is-primary">
         <p class="title">${movieDetails.Awards}</p>
         <p class="subtitle">Awards</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value="${dollars}" class="notification is-primary">
         <p class="title">${movieDetails.BoxOffice}</p>
         <p class="subtitle">Box Office</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value="${metascore}" class="notification is-primary">
         <p class="title">${movieDetails.Metascore}</p>
         <p class="subtitle">Metascore</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value="${imdbRating}" class="notification is-primary">
         <p class="title">${movieDetails.imdbRating}</p>
         <p class="subtitle">IMDB Rating</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value="${imdbVotes}" class="notification is-primary">
         <p class="title">${movieDetails.imdbVotes}</p>
         <p class="subtitle">IMDB Votes</p>
     </article>
